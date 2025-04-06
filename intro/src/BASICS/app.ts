2cb398b23d936f0404edb1a84f52ad4e0043a894
YourGitHubUsername <your-email@example.com>
2025-04-06 09:55:40 +0530

    basic primitive

---
+++ intro/src/BASICS/app.ts
@@ -0,0 +1,25 @@
+// ======================================== BASICS ========================================
+
+// String
+const Name: string = "someStriong";
+
+// Number
+const age: number = 21;
+
+// Boolean
+const isOnline: boolean = false;
+
+// Any
+const anything: any = "this can be any typeeeeeee" + 1123 + true;
+
+// ======================================== PROBLEM 1: Create a user profile using basic types ===================================
+
+const userName: string = "guruuuu";
+const Age: number = 21;
+const isStudent: boolean = true;
+const favQuote: string = "stay foolish stay hungry";
+
+let anyy: any = "i am guruuuuuuuu";
+anyy = 1232;
+
+// ======================================== BASICS PRIMITIVE TYPES END ========================================
