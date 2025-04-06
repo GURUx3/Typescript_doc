d3d9a721c126c5855eccaf091dc2b35182bfb72e
YourGitHubUsername <your-email@example.com>
2025-04-06 09:58:49 +0530

    files are deleted app.js and app.ts

--- intro/src/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function setAppPreference(mode, language) {
-    const langDisplay = language === "NA" ? "Not Preferred To Say" : language;
-    return `You are using ${mode} mode in ${langDisplay}.`;
-}
-const result = setAppPreference("Dark", "Tamil");
-console.log(result); // 👉 "You are using Dark mode in Tamil."
-const result2 = setAppPreference("Light", "NA");
-console.log(result2); // 👉 "You are using Light mode in Not Preferred To Say."
